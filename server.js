78b003d219005f808008723b980c21e4e0288903
bodeleta <bodeleta73@gmail.com>
2024-11-08 05:40:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors'); // Mengimpor CORS
 const app = express();
+
+// Menggunakan CORS untuk mengizinkan permintaan dari domain lain
+app.use(cors({
+  origin: 'https://s3.smu.edu', // Atau bisa '*' untuk mengizinkan semua domain
+  methods: ['GET'],
+  allowedHeaders: ['Content-Type']
+}));
 
 // Menyajikan file XML saat URL /video/ diakses
 app.get('/video', (req, res) => {
   const xmlFilePath = path.join(__dirname, 'xml-files', 'video.xml');
-
+  
   // Cek apakah file ada sebelum mengirimnya
   fs.exists(xmlFilePath, (exists) => {
     if (exists) {
       // Menambahkan header Content-Type untuk file XML
       res.setHeader('Content-Type', 'application/xml');
       res.sendFile(xmlFilePath);
-    } else {
-      res.status(404).send('File not found');
-    }
-  });
-});
-
-// Menangani permintaan file lain yang perlu konfigurasi header khusus
-app.get('/another-file', (req, res) => {
-  const anotherFilePath = path.join(__dirname, 'xml-files', 'anotherfile.xml');
-
-  fs.exists(anotherFilePath, (exists) => {
-    if (exists) {
-      res.setHeader('Content-Type', 'application/xml');
-      res.sendFile(anotherFilePath);
     } else {
       res.status(404).send('File not found');
     }
